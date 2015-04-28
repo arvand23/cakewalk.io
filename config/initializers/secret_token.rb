dc33f929dafd2b511b08127c2198307cd6fc5b21
Arvand Alviri <arvando@Arvands-MacBook-Pro.local>
2015-04-28 17:17:19 -0600

    users and bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cakewalkio::Application.config.secret_key_base = '4311734d294b3c65759006652db07a48eb72f5eb5d7eba048cd66cdbc1f5b8ee5aabbce401e49b24aa6c83680623dbcbe73fb593f52796e0c210820cfe93e5f5'
