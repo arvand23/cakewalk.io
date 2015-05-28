f505c6055c8f6ce335b5ecbeef29f02b0d0fc430
Paulo Abreu <paulo.abreu@clustercube.com>
2015-05-28 10:00:35 +0100

    Admin tables grouped by recording_id

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -8,22 +8,28 @@ class Admin::EventsController < ApplicationController
   def screen_shares
     if params[:id]
       @screen_shares = ScreenShare.where(test_id: params[:id])
-                         .order(created_at: :desc)
-                         .paginate(:page => params[:page], :per_page => 30)
+                                  .group(:recording_id)
+                                  .order(created_at: :desc)
+                                  .paginate(:page => params[:page], :per_page => 30)
     else
-      @screen_shares = ScreenShare.order(created_at: :desc)
-                           .paginate(:page => params[:page], :per_page => 30)
+      @screen_shares = ScreenShare.group(:recording_id)
+                                  .order(created_at: :desc)
+                                  .paginate(:page => params[:page], :per_page => 30)
     end
   end
 
   def screen_share_events
     if params[:id]
-      @screen_share_events = ScreenShareEvent.where(screen_share_id: params[:id])
-                                 .order(:created_at => :desc)
-                                 .paginate(:page => params[:page], :per_page => 30)
+      @screen_share_events = ScreenShareEvent.joins(:screen_share)
+                                             .where(screen_share_id: params[:id])
+                                             .group('screen_shares.recording_id')
+                                             .order(:created_at => :desc)
+                                             .paginate(:page => params[:page], :per_page => 30)
     else
-      @screen_share_events = ScreenShareEvent.order(:created_at => :desc)
-                                 .paginate(:page => params[:page], :per_page => 30)
+      @screen_share_events = ScreenShareEvent.joins(:screen_share)
+                                             .group('screen_shares.recording_id')
+                                             .order(:created_at => :desc)
+                                             .paginate(:page => params[:page], :per_page => 30)
     end
   end
 end
