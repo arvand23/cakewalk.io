3422253f929d03dc01a44a78550c736324041386
Paulo Abreu <paulo.abreu@clustercube.com>
2015-05-28 12:01:10 +0100

    Admin tables: replace group by order

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -8,11 +8,11 @@ class Admin::EventsController < ApplicationController
   def screen_shares
     if params[:id]
       @screen_shares = ScreenShare.where(test_id: params[:id])
-                                  .group(:recording_id)
+                                  .order(:recording_id)
                                   .order(created_at: :desc)
                                   .paginate(:page => params[:page], :per_page => 30)
     else
-      @screen_shares = ScreenShare.group(:recording_id)
+      @screen_shares = ScreenShare.order(:recording_id)
                                   .order(created_at: :desc)
                                   .paginate(:page => params[:page], :per_page => 30)
     end
@@ -22,12 +22,12 @@ class Admin::EventsController < ApplicationController
     if params[:id]
       @screen_share_events = ScreenShareEvent.joins(:screen_share)
                                              .where(screen_share_id: params[:id])
-                                             .group('screen_shares.recording_id')
+                                             .order('screen_shares.recording_id')
                                              .order(:created_at => :desc)
                                              .paginate(:page => params[:page], :per_page => 30)
     else
       @screen_share_events = ScreenShareEvent.joins(:screen_share)
-                                             .group('screen_shares.recording_id')
+                                             .order('screen_shares.recording_id')
                                              .order(:created_at => :desc)
                                              .paginate(:page => params[:page], :per_page => 30)
     end
