94c314a12f432d1d19f00373eee5e5990b028901
Paulo Abreu <paulo.abreu@clustercube.com>
2015-06-04 10:10:56 +0100

    Order admin tables as desc.

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -8,11 +8,11 @@ class Admin::EventsController < ApplicationController
   def screen_shares
     if params[:id]
       @screen_shares = ScreenShare.where(test_id: params[:id])
-                                  .order(:recording_id)
+                                  .order(recording_id: :desc)
                                   .order(created_at: :desc)
                                   .paginate(:page => params[:page], :per_page => 30)
     else
-      @screen_shares = ScreenShare.order(:recording_id)
+      @screen_shares = ScreenShare.order(recording_id: :desc)
                                   .order(created_at: :desc)
                                   .paginate(:page => params[:page], :per_page => 30)
     end
@@ -22,13 +22,13 @@ class Admin::EventsController < ApplicationController
     if params[:id]
       @screen_share_events = ScreenShareEvent.joins(:screen_share)
                                              .where(screen_share_id: params[:id])
-                                             .order('screen_shares.recording_id')
-                                             .order(:created_at => :desc)
+                                             .order('screen_shares.recording_id' => :desc)
+                                             .order(created_at: :desc)
                                              .paginate(:page => params[:page], :per_page => 30)
     else
       @screen_share_events = ScreenShareEvent.joins(:screen_share)
-                                             .order('screen_shares.recording_id')
-                                             .order(:created_at => :desc)
+                                             .order('screen_shares.recording_id' => :desc)
+                                             .order(created_at: :desc)
                                              .paginate(:page => params[:page], :per_page => 30)
     end
   end
