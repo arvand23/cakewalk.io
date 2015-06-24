b9183961d4ee7d59cb445a73b17b056acd4d608a
Arvand Alviri <aalviri@gmail.com>
2015-06-23 17:34:28 -0700

    notif subj

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -9,6 +9,6 @@ class NotificationMailer < ActionMailer::Base
   def recordingready(currentuser, current_test_url)
   	@current_user = currentuser
   	@current_test_url = current_test_url
-  	mail(to: @current_user.email, subject: '#{@current_test_url} User Test Recording is Ready!')
+  	mail(to: @current_user.email, subject: 'Cakewalk User Test Recording is Ready!')
   end
 end
