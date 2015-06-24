5d1ebe02e4cc4c6fcc4073274a3a4b26544e7354
Arvand Alviri <aalviri@gmail.com>
2015-06-24 00:42:13 -0700

    validate email format

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, 
+						format: { with: VALID_EMAIL_REGEX },
+						uniqueness: { case_sensitive: false } #infers that uniqueness should be true as well
+
   has_many :tests
 
 end
